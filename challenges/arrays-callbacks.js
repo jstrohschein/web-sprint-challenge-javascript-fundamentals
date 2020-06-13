897ee82374b5d26e94e306e732c67d9358d00df1
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-06-13 14:53:25 -0400

    Finished all challenges and stretch goals

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,7 +20,22 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+ const displayNames = [];
+ console.log(displayNames);
+
+ 
+
+
+zooAnimals.forEach(setNames = (animal) => {
+
+  return displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`);
+  
+
+  // names.push(zooAnimals.animal_name);
+  // names.push(zooAnimals.scientific_name);
+
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,24 +44,65 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+
+
+
+
+
+
+const lowCaseAnimalNames = zooAnimals.map((animal) => {
+  return animal.animal_name.toLowerCase();
+})
 console.log(lowCaseAnimalNames);
+
+
+
+
 
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+
+
+
+
+const lowPopulationAnimals = zooAnimals.filter((animal) => {
+
+  return animal.population < 5;
+
+})
 console.log(lowPopulationAnimals);
+
+
+
+
+
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+
+
+
+
+
+const populationTotal = zooAnimals.reduce((total, animal) => {
+
+    return total += animal.population;
+
+}, 0);
+
+
+
 console.log(populationTotal);
+
+
+
+
 
 
 // ==== Callbacks ====  
@@ -59,17 +115,48 @@ console.log(populationTotal);
 */
 
 
+
+
+consume = (a, b, cb) => cb(a, b);
+
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+// let add = function (x, y){
+//   return x + y;
+// };
+
+add = (x, y) => x + y;
+
+// let multiply = function(x, y){
+//   return x * y;
+// };
+
+multiply = (x, y) => x * y;
+
+// let greeting = function(firstName, lastName){
+//   return `Hello ${firstName} ${lastName}, nice to meet you!`;
+// }
+
+greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
+
+
+
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+
+
+
+
 
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,23 @@ myFunction();
 
 // Explanation: 
 
+/*The nested function is able to access 'internal' because, with closure, a function can look outside of its scope and use variables that are defined in a higher-order function. In this case, 'internal' was defined in 'myFunction' which is a parent to 'nestedFunction' so 'nestedFunction' can look outside it's scope and see that internal is defined within myFunction and execute the 'console.log' using 'internal' */
+
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+summation = (num) => {
+  
+  let x = 0;
+
+  for (let i = 1; i <= num; i++){
+    x += i;
+  }
+
+  return x;
+  
+};
+
+console.log(summation(4));
